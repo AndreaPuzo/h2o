f6c1d5157552bbe1717663cc090535253014fd84
Andpuv <90248245+AndreaPuzo@users.noreply.github.com>
2023-03-21 23:46:16 +0100

    Update __entry.c

--- src/__entry.c
+++ src/__entry.c
@@ -5,52 +5,85 @@
 
 int main (int argc, char ** argv)
 {
-  if (argc < 2) {
-    fprintf(stderr, "fatal: no input\n") ;
-    fprintf(stderr, "usage: %s INPUT -o OUTPUT\n", argv[0]) ;
-    exit(EXIT_FAILURE) ;
+  if (0 == argc)
+    abort() ;
+  
+  if (1 == argc) {
+    fprintf(stderr, "usage: %s [<option>...] <input>\n", argv[0]) ;
+    fprintf(stderr, "usage: %s -h|--help\n", argv[0]) ;
+    exit(EXIT_SUCCESS) ;
   }
 
   struct {
-    int    type   ;
     char * input  ;
     char * output ;
   } opts ;
 
-  opts.type   = 0    ;
   opts.input  = NULL ;
   opts.output = NULL ;
 
   for (int i = 1 ; i < argc ; ++i) {
-    if (0 == strcmp(argv[i], "-o")) {
-      if (i + 1 == argc) {
-        fprintf(stderr, "fatal: missing argument for option `%s`\n", argv[i]) ;
-        exit(EXIT_FAILURE) ;
+    if (0 == strcmp(argv[i], "-v") || 0 == strcmp(argv[i], "--ver")) {
+      fprintf(stderr, "h2o %d.%d.%d\n", H2O_VERSION_MAJOR, H2O_VERSION_MINOR, H2O_VERSION_PATCH) ;
+      exit(EXIT_SUCCESS) ;
+    }
+    
+    if (0 == strcmp(argv[i], "-h") || 0 == strcmp(argv[i], "--help")) {
+      fprintf(stderr, "usage: %s [<option>...] <input>\n", argv[0]) ;
+      fprintf(stderr,
+        "options:\n"
+        "  -h, --help         --- Print this page.\n"
+        "  -v, --ver          --- Print the version.\n"
+        "  -o, --out <output> --- Set the output file.\n"
+      ) ;
+      exit(EXIT_SUCCESS) ;
+    }
+    
+    if (0 == strcmp(argv[i], "-o") || 0 == strcmp(argv[i], "--out")) {
+      if (++i == argc) {
+        opts.output = argv[i] ;
       }
-
-      ++i ;
-
-      opts.output = argv[i] ;
     } else {
-#ifdef _EMIT_WARNINGS
-      if (NULL != opts.input)
+      if (NULL != opts.input) {
         fprintf(stderr, "warning: ignored input `%s`\n", argv[i]) ;
-      else
-#endif
+      } else {
         opts.input = argv[i] ;
+      }
     }
   }
 
   if (NULL == opts.input) {
     fprintf(stderr, "fatal: no input\n") ;
-    fprintf(stderr, "usage: %s INPUT -o OUTPUT\n", argv[0]) ;
+    fprintf(stderr, "usage: %s [<option>...] <input>\n", argv[0]) ;
     exit(EXIT_FAILURE) ;
   }
 
-  char * default_output = "a.o" ;
+  char default_output [256 + 1] ;
+  int i ;
+  int c = 'a' ;
+  
+  for (i = 0 ; i < 256 ;) {
+    if ('z' == c) {
+      default_output[i] = c ;
+      c = 'a' ;
+      ++i ;
+    } else {
+      default_output[i] = c++ ;
+    }
+    
+    FILE * fp = fopen(default_output, "r") ;
 
-  if (NULL == opts.output)
+    if (NULL == fp)
+      break ;
+  }
+  
+  default_output[i++] = '.' ;
+  default_output[i++] = 'o' ;
+  default_output[i] = 0 ;
+
+  if (NULL == opts.output) {
     opts.output = default_output ;
+  }
 
   if (H2O_SUCCESS != h2o(opts.input, opts.output)) {
     fprintf(stderr, "Something has gone wrong...\n") ;
