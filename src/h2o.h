d2d10b01939194a4282def743a97bf6363fcca3d
Andpuv <90248245+AndreaPuzo@users.noreply.github.com>
2023-03-21 23:47:40 +0100

    Update h2o.h

--- src/h2o.h
+++ src/h2o.h
@@ -1,6 +1,16 @@
 #ifndef _H2O_H
 # define _H2O_H
 
+# define H2O_VERSION_MAJOR 0
+# define H2O_VERSION_MINOR 0
+# define H2O_VERSION_PATCH 0
+# define H2O_VERSION            \
+  (                             \
+    (H2O_VERSION_MAJOR << 16) | \
+    (H2O_VERSION_MINOR <<  8) | \
+    (H2O_VERSION_PATCH <<  0)   \
+  )
+
 # define H2O_SUCCESS 0
 # define H2O_FAILURE 1
 
